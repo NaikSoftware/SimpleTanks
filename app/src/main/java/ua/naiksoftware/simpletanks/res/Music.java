75689055e3a9393b5a3793a807941fcea02e434b
Naik <naiksav4enko@gmail.com>
2015-07-27 15:54:26 +0300

    Приведен в порядок класс музыки

--- app/src/main/java/ua/naiksoftware/simpletanks/res/Music.java
+++ app/src/main/java/ua/naiksoftware/simpletanks/res/Music.java
@@ -9,27 +9,34 @@ import android.util.SparseIntArray;
 
 import java.util.HashMap;
 
-import ua.naiksoftware.simpletanks.Log;
-
 /**
  * Created by Naik on 25.07.15.
+ *
+ *      Utility for easy music and sounds thread safe playback.
+ *      Usage: in main activity in onCreate method call Music.init(getContext()),
+ *      in onDestroy call Music.dispose(). After this you can play music and sounds anywhere:
+ *      Music.playMusic(keyObject, R.raw.test)
  */
 public class Music {
 
-    private static final String TAG = Music.class.getSimpleName();
-
     private static Context context;
 
-    /* Music */
+    /** Music mapping: key -> (rawID -> MediaPlayer) */
     private static final HashMap<Object, SparseArray<MediaPlayer>> musicsMap = new HashMap<Object, SparseArray<MediaPlayer>>();
 
-    /* Sound */
     private static SoundPool soundPool;
-    private static SparseIntArray loadedSounds = new SparseIntArray(); // rawID -> soundID
-    private static final HashMap<Object, SparseIntArray> soundsMap = new HashMap<Object, SparseIntArray>(); // key -> (soundID -> playID)
 
+    /** Loaded sounds mapping: rawID -> soundID */
+    private static final SparseIntArray loadedSounds = new SparseIntArray();
+
+    /** Play sounds mapping: key -> (soundID -> playID) */
+    private static final HashMap<Object, SparseIntArray> soundsMap = new HashMap<Object, SparseIntArray>();
+
+    /**
+     * You must call this method before using class.
+     * @param context
+     */
     public static void init(Context context) {
-        Log.e(TAG, "Init");
         Music.context = context;
         soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
     }
@@ -48,7 +55,13 @@ public class Music {
         return music;
     }
 
-    public static void playMusic(final Object key, final int rawID, final boolean loop) {
+    /**
+     * Run playback long music file, for short see Music.playSound
+     * @param key - key for assign player to specified object
+     * @param rawID - R.raw.[you music in /res/raw directory]
+     * @param loop - if true - play looping, if already playing - doing nothing
+     */
+    public static synchronized void playMusic(Object key, int rawID, boolean loop) {
         MediaPlayer music = getMusic(key, rawID);
         if (loop) {
             if (!music.isPlaying()) {
@@ -63,32 +76,39 @@ public class Music {
         }
     }
 
-    public static void stopMusic(Object key, int rawID) {
+    /**
+     * Stop music by id for key object
+     * @param key - key object
+     * @param rawID - R.raw.[...]
+     */
+    public static synchronized void stopMusic(Object key, int rawID) {
         MediaPlayer sound = getMusic(key, rawID);
         sound.stop();
     }
 
+    /**
+     * Run playback sort music (sound). Sound will be loaded into memory.
+     * @param key - key for assign player to specified object
+     * @param rawID - R.raw.[...]
+     * @param volume - volume for left and right channels
+     * @param loop - if true - play looping, if already playing - doing nothing
+     */
     public static synchronized void playSound(final Object key, final int rawID, final float volume, final boolean loop) {
-        //Log.e(TAG, "playSound(" + key + ", " + rawID + ", " + volume + ", " + loop + ")");
         SparseIntArray playingSounds = soundsMap.get(key);
         if (playingSounds == null) {
             playingSounds = new SparseIntArray();
             soundsMap.put(key, playingSounds);
-            Log.e(TAG, "create new playingSounds list");
         }
         int soundID = loadedSounds.get(rawID, -1);
         if (soundID == -1) {
-            Log.e(TAG, "start load new sound");
             final long startLoad = System.currentTimeMillis();
             soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
                 @Override
                 public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
                     loadedSounds.put(rawID, sampleId);
                     long loadTime = System.currentTimeMillis() - startLoad;
-                    Log.e(TAG, "loaded rawID=" + rawID + ", soundID=" + sampleId + ", time=" + loadTime);
                     if (loadTime < 500) {
                         // Проиграть звук только если прошло не более 500 мс после начала загрузки
-                        Log.e(TAG, "start recursive play loaded sound");
                         playSound(key, rawID, volume, loop);
                     }
                 }
@@ -100,27 +120,29 @@ public class Music {
         int playID = playingSounds.get(soundID, -1);
         if (playID == -1) { // Если этот звук еще не играет - играем.
             playingSounds.put(soundID, soundPool.play(soundID, volume, volume, 0, loops, 1));
-            //Log.e(TAG, "start soundID=" + soundID + " as playID=" + playingSounds.get(soundID));
-        } else { // Иначе остановим его и проиграем.
-            //Log.e(TAG, "sound " + soundID + " playing now as playID=" + playID + ", stopping");
+        } else if (!loop){ // Иначе остановим его и проиграем (если запущено в цикле, ничего не делаем).
             soundPool.stop(playID);
             playingSounds.put(soundID, soundPool.play(soundID, volume, volume, 0, loops, 1));
-            //Log.e(TAG, "restart soundID=" + soundID + " as playID=" + playingSounds.get(soundID));
         }
     }
 
+    /**
+     * Stop sound by rawID for specified object key.
+     * @param key - object key
+     * @param rawID - R.raw.[...]
+     */
     public static synchronized void stopSound(Object key, int rawID) {
-        //Log.e(TAG, "stopSound(" + key + ", " + rawID + ")");
         int soundID = loadedSounds.get(rawID);
         SparseIntArray playingSounds = soundsMap.get(key);
         int playID = playingSounds.get(soundID);
         soundPool.stop(playID);
         playingSounds.delete(soundID);
-        //Log.e(TAG, "stopped rawID=" + rawID + ", soundID=" + soundID + ", playID=" + playID);
     }
 
+    /**
+     * You must call this method after using class.
+     */
     public static synchronized void dispose() {
-        Log.e(TAG, "Dispose");
         // Stop music.
         for (SparseArray<MediaPlayer> musics : musicsMap.values()) {
             for (int i = 0, size = musics.size(); i < size; i++) {
